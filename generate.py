5d28a7ea52db04c55a6a240a2543c195e066d42c
benoriol <benet.oriol.sabat@gmail.com>
2018-10-08 12:06:53 +0200

    Rotation working

--- generate.py
+++ generate.py
@@ -6,11 +6,6 @@ import random
 background_path = 'data/refugee-camp-before-data.jpg'
 # Path to foreground pattern
 house_path = 'data/casa5.jpg'
-# Mininum and maximum number of rows and columns
-# battery_boundaries = [[3, 7], [2, 5]]
-
-# nrows = random.choice(range(battery_boundaries[0][0], battery_boundaries[0][1]+1))
-# ncols = random.choice(range(battery_boundaries[1][0], battery_boundaries[1][1]+1))
 
 battery_shape = [3, 7] # shape of the houses disposition
 n_houses = 15 # number of houses per battery
@@ -23,7 +18,7 @@ house = cv2.imread(house_path)
 for i in range(n_outputs):
 
     output = background.copy()
-    mask = np.zeros((background.shape[0], background.shape[1], 3), np.uint8)
+    mask = np.zeros((background.shape[0], background.shape[1]), np.uint8)
 
     possible_positions = []
     for x in range(battery_shape[0]):
@@ -37,14 +32,14 @@ for i in range(n_outputs):
         possible_positions.remove(p)
 
     houses = np.zeros((battery_shape[0]*house.shape[0], battery_shape[1]*house.shape[1], 3), np.uint8)
-    _mask = houses.copy()
+    _mask = np.zeros((battery_shape[0]*house.shape[0], battery_shape[1]*house.shape[1]), np.uint8)
 
     for p in positions:
         x = house.shape[0]*p[0]
         y = house.shape[1]*p[1]
 
         houses[x: x+house.shape[0], y: y+house.shape[1], :] = house
-        _mask[x: x+house.shape[0], y: y+house.shape[1], :] = 255
+        _mask[x: x+house.shape[0], y: y+house.shape[1]] = 255
 
     rotation = random.choice(range(-90, 90))
 
@@ -57,10 +52,6 @@ for i in range(n_outputs):
     # Affine transformation
     rotated = cv2.warpAffine(houses, M, (background.shape[0], background.shape[1]))
     _mask_rotated = cv2.warpAffine(_mask, M, (background.shape[0], background.shape[1]))
-
-    cv2.imshow('output', _mask_rotated)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
     # Getting the part o the image with houses
     x0 = 0
@@ -93,6 +84,10 @@ for i in range(n_outputs):
     cut_rotated = rotated[x0:x1, y0:y1]
     cut__mask = _mask_rotated[x0:x1, y0:y1]
 
+    cv2.imshow('cut rot', cut_rotated)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
     cv2.imshow('output', cut__mask)
     cv2.waitKey()
     cv2.destroyAllWindows()
@@ -102,23 +97,18 @@ for i in range(n_outputs):
     start_y = random.choice(range(0, background.shape[1] - cut_rotated.shape[1]))
 
     mask[start_x:start_x+cut__mask.shape[0], start_y:start_y+cut__mask.shape[1]] = cut__mask
-    output[start_x:start_x+cut_rotated.shape[0], start_y:start_y+cut_rotated.shape[1]] = cut_rotated
+    houses_big = np.zeros((background.shape[0], background.shape[1], background.shape[2]), np.uint8)
+    houses_big[start_x:start_x + cut__mask.shape[0], start_y:start_y + cut__mask.shape[1], :] = cut_rotated
 
-    ones = np.ones((mask.shape[0], mask.shape[1], 3), np.uint8)*255
-    ret,mask = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
-    output[:,:,0] = mask[:,:,0]*output[:,:,0] + (ones[:,:,0] - mask[:,:,0])*background[:,:,0]
-    output[:,:,1] = mask[:,:,1]*output[:,:,1] + (ones[:,:,1] - mask[:,:,1])*background[:,:,1]
-    output[:,:,2] = mask[:,:,2]*output[:,:,2] + (ones[:,:,2] - mask[:,:,2])*background[:,:,2]
 
-    cv2.imshow('output', output)
+    for i in range(output.shape[2]):
+        output[:, :, i] = output[:, :, i] * (1-mask/255)
+
+    output = output + houses_big
+
+    cv2.imshow('output final', output)
     cv2.waitKey()
     cv2.destroyAllWindows()
-
-    # output = output * (1-mask/255)
-    #
-    # cv2.imshow('output', output)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
 
 
     outpath_ = background_path.split('.')
