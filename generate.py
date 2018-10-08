c703995884dfa6efa13653a72c86c9acb2bd7536
benoriol <benet.oriol.sabat@gmail.com>
2018-10-08 11:40:16 +0200

    house extraction bug fixed

--- generate.py
+++ generate.py
@@ -14,7 +14,7 @@ house_path = 'data/casa5.jpg'
 
 battery_shape = [3, 7] # shape of the houses disposition
 n_houses = 15 # number of houses per battery
-n_outputs = 1 # number of house dispositions
+n_outputs = 15 # number of house dispositions
 
 background = cv2.imread(background_path)
 house = cv2.imread(house_path)
@@ -45,36 +45,57 @@ for i in range(n_outputs):
 
         houses[x: x+house.shape[0], y: y+house.shape[1], :] = house
         _mask[x: x+house.shape[0], y: y+house.shape[1], :] = 255
-        pass
 
     rotation = random.choice(range(-90, 90))
+
+    # Definir Rotació
     M = cv2.getRotationMatrix2D((0,0), rotation, 1)
+
+    # Definir translació
     M[:,2] = np.array([houses.shape[0], houses.shape[1]])
+
+    # Affine transformation
     rotated = cv2.warpAffine(houses, M, (background.shape[0], background.shape[1]))
     _mask_rotated = cv2.warpAffine(_mask, M, (background.shape[0], background.shape[1]))
 
-    edge_rows = [-1, -1]
-    edge_columns = [-1, -1]
-    for row in range(0, rotated.shape[0]):
-        if edge_rows[0] == -1:
-            if np.sum(rotated[row,:]) != 0:
-                edge_rows[0] = row - 1 if row > 0 else row
-        elif edge_rows[0] != -1 and edge_rows[1] == -1:
-            if np.sum(rotated[row,:]) == 0:
-                edge_rows[1] = row
-        else:
+    cv2.imshow('output', _mask_rotated)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # Getting the part o the image with houses
+    x0 = 0
+    x1 = _mask_rotated.shape[0]-1
+
+    y0 = 0
+    y1 = _mask_rotated.shape[1] - 1
+    
+    for x in range(x1+1):
+        if x0 == 0 and np.sum(_mask_rotated[x]) != 0:
+            x0 = x
+            
+        if x1 == _mask_rotated.shape[0]-1 and np.sum(_mask_rotated[x1-x]) != 0:
+            x1 = x1-x
+            
+        if (x0 != 0 and x1 != _mask_rotated.shape[0]-1):
             break
-    for column in range(0, rotated.shape[1]):
-        if edge_columns[0] == -1:
-            if np.sum(rotated[:,column]) != 0:
-                edge_columns[0] = column-1 if column > 0 else column
-        elif edge_columns[0] != -1 and edge_columns[1] == -1:
-            if np.sum(rotated[:,column]) == 0:
-                edge_columns[1] = column
-        else:
+
+    for y in range(y1 + 1):
+        if np.sum(_mask_rotated[:, y]) != 0 and y0 == 0:
+            y0 = y
+
+        if  y1 == _mask_rotated.shape[1] - 1 and np.sum(_mask_rotated[:, y1 - y]) != 0:
+            y1 = y1 - y
+
+        if y0 != 0 and y1 != _mask_rotated.shape[1]-1 and x0 != 0 and x1 != _mask_rotated.shape[0]-1:
             break
-    cut_rotated = rotated[edge_rows[0]:edge_rows[1], edge_columns[0]:edge_columns[1]]
-    cut__mask = _mask_rotated[edge_rows[0]:edge_rows[1], edge_columns[0]:edge_columns[1]]
+
+
+    cut_rotated = rotated[x0:x1, y0:y1]
+    cut__mask = _mask_rotated[x0:x1, y0:y1]
+
+    cv2.imshow('output', cut__mask)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     # start of battery (in pixels)
     start_x = random.choice(range(0, background.shape[0] - cut_rotated.shape[0]))
@@ -82,14 +103,23 @@ for i in range(n_outputs):
 
     mask[start_x:start_x+cut__mask.shape[0], start_y:start_y+cut__mask.shape[1]] = cut__mask
     output[start_x:start_x+cut_rotated.shape[0], start_y:start_y+cut_rotated.shape[1]] = cut_rotated
+
     ones = np.ones((mask.shape[0], mask.shape[1], 3), np.uint8)*255
     ret,mask = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
     output[:,:,0] = mask[:,:,0]*output[:,:,0] + (ones[:,:,0] - mask[:,:,0])*background[:,:,0]
     output[:,:,1] = mask[:,:,1]*output[:,:,1] + (ones[:,:,1] - mask[:,:,1])*background[:,:,1]
     output[:,:,2] = mask[:,:,2]*output[:,:,2] + (ones[:,:,2] - mask[:,:,2])*background[:,:,2]
+
+    cv2.imshow('output', output)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # output = output * (1-mask/255)
+    #
     # cv2.imshow('output', output)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
+
 
     outpath_ = background_path.split('.')
     folder = outpath_[0].split('/')
