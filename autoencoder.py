e7feaafdb907b829c382ea8ad195a3613421f296
benoriol <benet.oriol.sabat@gmail.com>
2018-10-17 12:19:18 +0200

    first autoencoder architecture designed

--- autoencoder.py
+++ autoencoder.py
@@ -1,3 +1,8 @@
+'''
+    Implementing a convolutional autoencoder. https://lmb.informatik.uni-freiburg.de/people/ronneber/u-net/
+'''
+
+
 import torch
 import torch.nn as nn
 import torchvision
@@ -10,8 +15,6 @@ from glob import glob
 
 def load_data():
     data = []
-
-
 
     for d in glob("data/refugee-camp-before-data-out*-mask.jpg"):
         tgtim =torch.from_numpy(cv2.imread(d)/255).permute(2, 0, 1).float()
@@ -29,22 +32,26 @@ class Autoencoder(nn.Module):
         self.max22 = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
         self.upsample1 = nn.Upsample(size=(236, 353), mode='bilinear')
+        self.upsample2 = nn.Upsample(size=(473, 706), mode='bilinear')
 
         self.conv1_1 = nn.Conv2d(3, 16, 3)
         self.conv1_2 = nn.Conv2d(16, 16, 3)
         self.conv1_3 = nn.Conv2d(16, 16, 3)
 
-        self.conv1_4 = nn.Conv2d(16, 16, 3)
-        self.conv1_5 = nn.Conv2d(16, 16, 3)
-        self.conv1_6 = nn.Conv2d(16, 16, 3)
+        self.conv1_4 = nn.Conv2d(16, 32, 3)
+        self.conv1_5 = nn.Conv2d(32, 32, 3)
+        self.conv1_6 = nn.Conv2d(32, 32, 3)
 
-        self.conv2_1 = nn.Conv2d(16, 16, 3)
-        self.conv2_2 = nn.Conv2d(16, 16, 3)
-        self.conv2_3 = nn.Conv2d(16, 16, 3)
+        self.conv1_7 = nn.Conv2d(32, 64, 3)
+        self.conv1_8 = nn.Conv2d(64, 64, 3)
 
-        self.conv2_4 = nn.Conv2d(16, 16, 3)
+        self.conv2_1 = nn.Conv2d(64, 32, 3)
+        self.conv2_2 = nn.Conv2d(32, 32, 3)
+        self.conv2_3 = nn.Conv2d(32, 32, 3)
+
+        self.conv2_4 = nn.Conv2d(32, 16, 3)
         self.conv2_5 = nn.Conv2d(16, 16, 3)
-        self.conv2_6 = nn.Conv2d(16, 16, 3)
+        self.conv2_6 = nn.Conv2d(16, 1, 3)
 
 
 
@@ -52,47 +59,78 @@ class Autoencoder(nn.Module):
 
         print('inputsize', inp.size())
 
-        out = self.conv1(inp)
 
+        # Encoder
+        out = self.conv1_1(inp)
         out = self.relu(out)
-        out = self.conv2(out)
+        out = self.conv1_2(out)
         out = self.relu(out)
-        out = self.conv3(out)
+        out = self.conv1_3(out)
+        out = self.relu(out)
+
+        out = self.max22(out)
+
+        out = self.conv1_4(out)
+        out = self.relu(out)
+        out = self.conv1_5(out)
+        out = self.relu(out)
+        out = self.conv1_6(out)
         out = self.relu(out)
 
         out = self.max22(out)
 
-        print('max1', out.size())
-
-
-        out = self.conv4(out)
+        out = self.conv1_7(out)
         out = self.relu(out)
-        out = self.conv5(out)
+        out = self.conv1_8(out)
         out = self.relu(out)
-        out = self.conv6(out)
-        out = self.relu(out)
-
-        out = self.max22(out)
-        print('max2', out.size())
 
         out = self.upsample1(out)
 
-        print('upsampled', out.size())
+        out = self.conv2_1(out)
+        out = self.relu(out)
+        out = self.conv2_2(out)
+        out = self.relu(out)
+        out = self.conv2_3(out)
+        out = self.relu(out)
 
+        out = self.upsample2(out)
 
+        out = self.conv2_4(out)
+        out = self.relu(out)
+        out = self.conv2_5(out)
+        out = self.relu(out)
+        out = self.conv2_6(out)
+        out = self.relu(out)
 
+        print('out size', out.size())
         return out
 
 
 if __name__ == '__main__':
 
-    BATCH_SIZE = 1
+    N_EPOCHS = 1
+
+    BATCH_SIZE = 5
 
     data = load_data()
 
-    batch = data[0][0].unsqueeze(0)
+    autoenc = Autoencoder()
 
-    a = Autoencoder()
+    for e in range(N_EPOCHS):
+        n = 0
+        batch_ = []
+        for i, d in enumerate(data):
+            batch_.append(d)
+            if (i+1)%BATCH_SIZE == 0:
+                src_batch = torch.empty(BATCH_SIZE, d[0].size()[0], d[0].size()[1], d[0].size()[2])
+                print('src', src_batch.size())
 
-    out = a(batch)
-    pass
+                tgt_batch = torch.empty(BATCH_SIZE, d[1].size()[0], d[1].size()[1], d[1].size()[2])
+                print('hola')
+                # for j, x in enumerate(batch_):
+                #     src_batch[j] = x[0]
+                #     tgt_batch[j] = x[1]
+
+            # print('src', src_batch.size())
+
+
