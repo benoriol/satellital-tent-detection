1fbf8bd44e310d819fa44717ed0905d1a32d0638
mbalibrea <mbalibrea@opentrends.net>
2018-12-01 23:26:46 +0100

    multiple input houses

--- generate.py
+++ generate.py
@@ -7,63 +7,151 @@ import os
 import sys
 from tqdm import tqdm
 
-def getHouses(pathForHouses, pathForBackground, parameters):
+def isImage(file):
+    extensions = ['jpg', 'png', 'gif', 'tiff']
+    return file.split('.')[-1] in extensions
 
-    scales = [0]
-    for i in range(0, pathForHouses.count):
-        _house = cv2.imread(pathForHouses[i])
-        factor = math.sqrt(_house.shape[0]*_house.shape[1]/(background.shape[0]*background.shape[1]))
-        # print("\nsize factor (house/background): " + str(factor))
-        if '-s' in parameters:
-            _relation = float(parameters.get('-s'))/100
-            relation = _relation if _relation <= min(1, factor) else 0.2
+def paths(parameters):
+
+    pathForBackground = parameters.get('-bf') if '-bf' in parameters else 'data/refugee_camp_before_data.jpg'
+    pathForMasks = [""]
+    if '-hf' in parameters:
+        if os.path.isfile(parameters.get('-hf')) and isImage(parameters.get('-hf')):
+            pathForHouses = [parameters.get('-hf')]
+            pathForMasks = [pathForHouses[0].replace('.', '-mask.') if os.path.isfile(pathForHouses[0].replace('.', '-mask.')) else pathForHouses[0]]
+        elif os.path.isdir(parameters.get('-hf')):
+            _pathForHouses = [parameters.get('-hf') + '/' + file for file in os.listdir(parameters.get('-hf')) if (os.path.isfile(parameters.get('-hf')+'/'+file) and "mask" not in file)]
+            pathForHouses = [path for path in _pathForHouses if isImage(path)]
+            pathForMasks = pathForHouses.copy()
+            paths = [parameters.get('-hf') + '/' + file for file in os.listdir(parameters.get('-hf'))]
+            for i in range(len(pathForHouses)):
+                index = [a for a, s in enumerate(paths) if pathForHouses[i].split('.')[-2] + '-mask' in s]
+                pathForMasks[i] = paths[index[0]]
+
         else:
-            relation = 0.2 if factor > 0.2 else factor
-        resize = relation/factor
-        scales[i] = 1/resize if 1/resize < 1 else resize
-        # print("final scaling: " + str(scale))
-        house = cv2.resize(_house, (0,0), fx=scale, fy=scale)
-        house_mask = cv2.resize(_house_mask, (0,0), fx=scale, fy=scale)
+            return
+    else:
+        # Path to foreground pattern
+        pathForHouses = ['data/casa1.png']
+        pathForMasks = ['data/casa1-mask.png']
 
+    return pathForHouses, pathForMasks, pathForBackground
 
-    biggest_area = [0, 0, 0]
-    for i in range(1, pathForHouses.count):
-        shape = cv2.imread(pathForHouses[i]).shape
-        if house.shape[0]*house.shape[1] > biggest_area[0]*biggest_area[1]:
-            biggest_area = [house.shape[0], house.shape[1], i]
-    houses = np.zeros((biggest_area[0], biggest_area[1], 3, pathForHouses.count), np.uint8)
+def dimensions(numberofhouses):
+    width = np.uint8(math.ceil(math.sqrt(numberofhouses)))
+    left = numberofhouses - width
+    i = 0
+    dimension = np.zeros(numberofhouses*2)
+    dimension[i] = width
+    while left > dimension[i]:
+        width = math.ceil(math.sqrt(left))
+        left = left - width
+        i = i + 1
+        dimension[i] = width
+    dimension[i+1] = left
 
+    return np.trim_zeros(dimension)
 
+def crop(image):
+
+    x0 = 0
+    x1 = image.shape[0] - 1
+
+    y0 = 0
+    y1 = image.shape[1] - 1
+
+    for x in range(x1+1):
+        if x0 == 0 and np.sum(image[x]) != 0:
+            x0 = x
+
+        if x1 == image.shape[0]-1 and np.sum(image[x1-x]) != 0:
+            x1 = x1-x
+
+        if (x0 != 0 and x1 != image.shape[0]-1):
+            break
+
+    for y in range(y1 + 1):
+        if np.sum(image[:, y]) != 0 and y0 == 0:
+            y0 = y
+
+        if  y1 == image.shape[1] - 1 and np.sum(image[:, y1 - y]) != 0:
+            y1 = y1 - y
+
+        if y0 != 0 and y1 != image.shape[1]-1 and x0 != 0 and x1 != image.shape[0]-1:
+            break
+    return [x0, x1, y0, y1]
+
+def getHouses(parameters):
+
+    pathForHouses, pathForMasks, pathForBackground = paths(parameters)
+    biggest_area = [0, 0]
+    background = cv2.imread(pathForBackground)
+    background_shape = background.shape
+    house_shape = np.zeros(2)
+    for i in range(len(pathForHouses)):
+
+        house_shape[0] = cv2.imread(pathForHouses[i]).shape[0]
+        house_shape[1] = cv2.imread(pathForHouses[i]).shape[1]
+        if '-s' in parameters and float(parameters.get('-s'))*max(house_shape[0], house_shape[1]) < max(background_shape[0], background_shape[1]):
+            house_shape[0] = float(parameters.get('-s'))*house_shape[0]
+            house_shape[1] = float(parameters.get('-s'))*house_shape[1]
+        else:
+            house_shape[0] = 0.8*house_shape[0]
+            house_shape[1] = 0.8*house_shape[1]
+        if house_shape[0]*house_shape[1] > biggest_area[0]*biggest_area[1]:
+            biggest_area = [house_shape[0], house_shape[1]]
+
+    dim = dimensions(len(pathForHouses)) # number of rows is .count, number of columns/row is dimensions[row]
+    houses = np.zeros((np.uint32(biggest_area[0]*np.amax(dim)*2), np.uint32(biggest_area[1]*len(dim)*2), 3), np.uint8)
+    masks = np.zeros((np.uint32(biggest_area[0]*np.amax(dim)*2), np.uint32(biggest_area[1]*len(dim)*2)), np.uint8)
+    if '-m' in parameters:
+        white_masks = masks.copy()
+    maximum = [0, 0]
+    _maximum = [0, 0]
+    i = 0
+    for row in range(len(dim)):
+        maximum[0] = 0
+        # maximum[1] el maxim actual, _maximum[1] el maxim anterior
+        for column in range(np.uint8(dim[row])):
+            _house = cv2.imread(pathForHouses[i])
+            _mask = cv2.imread(pathForMasks[i], cv2.IMREAD_GRAYSCALE)
+            if '-s' in parameters and float(parameters.get('-s'))*max(_house.shape[0], _house.shape[1]) < max(background.shape[0], background.shape[1]):
+                relation = float(parameters.get('-s'))
+            else:
+                relation = 0.8
+            house = cv2.resize(_house, (0,0), fx=relation, fy=relation)
+            mask = cv2.resize(_mask, (0,0), fx=relation, fy=relation)
+            if '-m' in parameters:
+                white_mask = cv2.resize(white_mask, (0,0), fx=relation, fy=relation)
+
+            _maximum[0] = maximum[0] + house.shape[0] + 5
+            houses[maximum[0]+5 : _maximum[0], _maximum[1]+5 : _maximum[1]+house.shape[1]+5] = house
+            masks[maximum[0]+5 : _maximum[0], _maximum[1]+5 : _maximum[1]+house.shape[1]+5] = mask
+            if '-m' in parameters:
+                white_masks[max[0]+5 : _maximum[0], _maximum[1]+5 : _maximum[1]+house.shape[1]+5] = white_mask
+            maximum[0] = _maximum[0]
+            maximum[1] = _maximum[1]+house.shape[1]+5 if _maximum[1]+house.shape[1]+5 > maximum[1] else maximum[1]
+            i = i + 1
+        _maximum[1] = maximum[1]
+
+    x0, x1, y0, y1 = crop(masks)
+    final_houses = houses[x0:x1, y0:y1]
+    final_masks = masks[x0:x1, y0:y1]
+    final_white_masks = white_masks[x0:x1, y0:y1] if '-m' in parameters else None
+
+    return final_houses, final_masks, final_white_masks
 
 
 def generate(parameters):
 
-    # house_path = [""]
-    # if '-hf' in parameters:
-    #     if os.path.isfile(parameters.get('-hf')):
-    #         house_path = [parameters.get('-hf')]
-    #     elif os.path.isdir(parameters.get('-hf')):
-    #         files = [file for file in os.listdir('data/') if os.path.isfile(file)]
-    #         for file in files:
-    #             house_path.append(file)
-    #     else:
-    #         return
-    # else:
-    #     # Path to foreground pattern
-    #     house_path = ['data/casa1.png']
-    #     house_mask_path = ['data/casa1-mask.png']
-
-    house_path = parameters.get('-hf') if '-hf' in parameters else 'data/casa1.png'
-    house_mask_path = house_path.replace('.', '-mask.') if os.path.isfile(house_path.replace('.', '-mask.')) else house_path
-
     # Path to the background image
     background_path = parameters.get('-bf') if '-bf' in parameters else 'data/refugee_camp_before_data.jpg'
+    house_path = parameters.get('-hf') if '-hf' in parameters else 'data/casa1.png'
 
     outpath_ = background_path.split('.')
     data = outpath_[0].split('/')
 
-    date = parameters.get('-date')
-    # date = f'{dt:%Y%m%d%H%M}'
+    date = parameters.get('-date').replace('/', '')
 
     if not os.path.exists(data[0] + '/' + date):
         os.makedirs(data[0] + '/' + date)
@@ -78,34 +166,8 @@ def generate(parameters):
     metadata = open(metadatapath, mode)
 
     background = cv2.imread(background_path)
-    _house = cv2.imread(house_path)
-    _house_mask = cv2.imread(house_mask_path, cv2.IMREAD_GRAYSCALE)
-    if '-m' in parameters:
-        _house_white_mask = np.ones(_house_mask.shape, np.uint8)*255
 
-    # factor = math.sqrt(_house.shape[0]*_house.shape[1]/(background.shape[0]*background.shape[1]))
-    # # print("\nsize factor (house/background): " + str(factor))
-    # if '-s' in parameters:
-    #     _relation = float(parameters.get('-s'))/100
-    #     relation = _relation if _relation <= 1 else 0.2
-    # else:
-    #     relation = 0.2 if factor > 0.2 else factor
-    # resize = relation/factor
-    # scale = 1/resize if 1/resize < 1 else resize
-    # # print("final scaling: " + str(scale))
-    # house = cv2.resize(_house, (0,0), fx=scale, fy=scale)
-    # house_mask = cv2.resize(_house_mask, (0,0), fx=scale, fy=scale)
-    # if '-m' in parameters:
-    #     house_white_mask = cv2.resize(_house_white_mask, (0,0), fx=scale, fy=scale)
-    if '-s' in parameters and float(parameters.get('-s'))*max(_house.shape[0], _house.shape[1]) < max(background.shape[0], background.shape[1]):
-        relation = float(parameters.get('-s'))
-    else:
-        relation = 0.5
-    house = cv2.resize(_house, (0,0), fx=relation, fy=relation)
-    house_mask = cv2.resize(_house_mask, (0,0), fx=relation, fy=relation)
-    if '-m' in parameters:
-        house_white_mask = cv2.resize(_house_white_mask, (0,0), fx=relation, fy=relation)
-
+    house, house_mask, house_white_mask = getHouses(parameters)
 
     max_houses_width = np.uint32(background.shape[0]/house.shape[0])
     max_houses_height = np.uint32(background.shape[1]/house.shape[1])
@@ -130,8 +192,8 @@ def generate(parameters):
                 _dimensions = np.uint32(dimensions.split('x'))
             battery_shape = _dimensions.copy() # shape of the houses disposition
         else:
-            battery_shape[0] = random.choice(range(2, max_houses_width))
-            battery_shape[1] = random.choice(range(2, max_houses_height))
+            battery_shape[0] = random.choice(range(0, max_houses_width))+1
+            battery_shape[1] = random.choice(range(0, max_houses_height))+1
         possible_positions = []
         for x in range(battery_shape[0]):
             for y in range(battery_shape[1]):
@@ -141,8 +203,7 @@ def generate(parameters):
         # let's decide
         n_houses = np.uint32(parameters.get('-h')) if '-h' in parameters else None
         if n_houses == None:
-
-            n_houses = random.choice(range(1, battery_shape[0]*battery_shape[1]-1))
+            n_houses = random.choice(range(0, battery_shape[0]*battery_shape[1]))+1
         elif n_houses > battery_shape[0]*battery_shape[1]:
             n_houses = battery_shape[0]*battery_shape[1]-1
         for x in range(n_houses):
@@ -196,31 +257,7 @@ def generate(parameters):
             aux_white_mask_rotated = cv2.warpAffine(aux_white_mask, M, (diagonal*2, diagonal*2))
 
         # Getting the part of the image with houses
-        x0 = 0
-        x1 = mask_rotated.shape[0] - 1
-
-        y0 = 0
-        y1 = mask_rotated.shape[1] - 1
-
-        for x in range(x1+1):
-            if x0 == 0 and np.sum(mask_rotated[x]) != 0:
-                x0 = x
-
-            if x1 == mask_rotated.shape[0]-1 and np.sum(mask_rotated[x1-x]) != 0:
-                x1 = x1-x
-
-            if (x0 != 0 and x1 != mask_rotated.shape[0]-1):
-                break
-
-        for y in range(y1 + 1):
-            if np.sum(mask_rotated[:, y]) != 0 and y0 == 0:
-                y0 = y
-
-            if  y1 == mask_rotated.shape[1] - 1 and np.sum(mask_rotated[:, y1 - y]) != 0:
-                y1 = y1 - y
-
-            if y0 != 0 and y1 != mask_rotated.shape[1]-1 and x0 != 0 and x1 != mask_rotated.shape[0]-1:
-                break
+        x0, x1, y0, y1 = crop(mask_rotated)
 
         cut_rotated = rotated[x0:x1, y0:y1]
         cut_mask = mask_rotated[x0:x1, y0:y1]
@@ -302,9 +339,9 @@ def switch(command, arg):
                   '-h': "number of houses (def.: random)",
                   '-d': "battery shape dimensions (write NNxMM) (if 0x0 adjusts to background; def.: random)",
                   '-r': "rotation of all the batteries (range 0-180º) (def.: random)",
-                  '-s': "scale house factor (def.: -)",
+                  '-s': "scale house factor (def.: 0.8)",
                   '-m': "output mask for input house(s) or mask for each house (def.: for each house)",
-                  '-hf': "input house file (pass 1 filename) (def. data/casa1.png)",
+                  '-hf': "input house file (pass 1 filename or 1 folder) (def. data/casa1.png)",
                   '-bf': "input background file (pass 1 filename) (def. data/refugee_camp_before_data.jpg)",
                   '-date': "enter the date. mandatory.",
                   '-help': "help"
