76f75acd237cbb0f9693364481733205786bc6a7
mbalibrea <mbalibrea@opentrends.net>
2018-12-27 14:27:53 +0100

    :)

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@
   l=10
   # CASA1.PNG
   python generate.py -hf data/casa1.png -bf data/refugee_camp_before_data.jpg -date $dt -l $l -r 80 -s 1.2 -d 20x5 -margin 0
-  python generate.py -hf data/casa1.png -bf data/Desert1.png -date $dt -l $l -s 3 -r 70 -d 0x0 -h 35 -margin 0
-  # python generate.py -hf data/casa1.png -bf data/Desert2.png no mola
-  # python generate.py -hf data/casa1.png -bf data/Desert3.png no mola
+  python generate.py -hf data/casa1.png -bf data/Desert1.png -date $dt -l $l -s 3 -r 70 -d 0x0 -h 35 -margin 0 -m
+  # python generate.py -hf data/casa1.png -bf data/Desert2.png no
+  # python generate.py -hf data/casa1.png -bf data/Desert3.png no
   python generate.py -hf data/casa1.png -bf data/Desert4.png -date $dt -l $l -s 2.2 -r 150 -d 0x0 -h 50 -margin 0
   python generate.py -hf data/casa1.png -bf data/desert5.png -date $dt -l $l -s 4 -r 160 -d 0x0 -margin 0
-  # python generate.py -hf data/casa1.png -bf data/Desert6.png no mola
+  # python generate.py -hf data/casa1.png -bf data/Desert6.png no
   python generate.py -hf data/casa1.png -bf data/Desert7.png -date $dt -l $l -s 1.5 -r 75 -d 0x0 -h 60 -margin 0
 
   # CASA2.PNG no mola
